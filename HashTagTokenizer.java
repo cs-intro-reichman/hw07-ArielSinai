f07db8ecbf6fda2ac68cf5d805950a37e562c72e
ArielSinai <ariel.sinai@post.runi.ac.il>
2024-02-03 23:02:41 +0200

    draft

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -3,9 +3,11 @@
 public class HashTagTokenizer {
 
 	public static void main(String[] args) {
-
+		
 		String hashTag = args[0];
+
 		String []dictionary = readDictionary("dictionary.txt");
+
 		breakHashTag(hashTag, dictionary);
 	}
 
@@ -15,12 +17,21 @@ public class HashTagTokenizer {
 		In in = new In(fileName);
 
 		// Your code here
+		for (int i=0; i<3000; i++) {
+			dictionary[i] = in.readString();
+		}
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
 		// Your code here
+		for (int i=0; i<dictionary.length; i++) {
+			if (word.equals(dictionary[i])) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
@@ -29,12 +40,19 @@ public class HashTagTokenizer {
         if (hashtag.isEmpty()) {
             return;
         }
- 
-        int N = hashtag.length();
-
-        for (int i = 1; i <= N; i++) {
 		
+		hashtag = hashtag.toLowerCase();
+        int N = hashtag.length();
+		
+        for (int i = 1; i <= N; i++) {
+			String prefix = hashtag.substring(0,i);
+			if (existInDictionary(prefix, dictionary)) {
+				System.out.println(prefix);
+				String sufix = hashtag.substring(i);
+				breakHashTag(sufix, dictionary);
+				return;
+			}
         }
-    }
 
+    }
 }
--- SpellChecker.java
+++ SpellChecker.java
@@ -3,19 +3,38 @@ public class SpellChecker {
 
 
 	public static void main(String[] args) {
-		String word = args[0];
-		int threshold = Integer.parseInt(args[1]);
-		String[] dictionary = readDictionary("dictionary.txt");
-		String correction = spellChecker(word, threshold, dictionary);
-		System.out.println(correction);
+
+		System.out.println(tail("hello"));
+
+		// String word = args[0];
+		// int threshold = Integer.parseInt(args[1]);
+		// String[] dictionary = readDictionary("dictionary.txt");
+		// String correction = spellChecker(word, threshold, dictionary);
+		// System.out.println(correction);
+
+
 	}
 
 	public static String tail(String str) {
 		// Your code goes here
+		if (str.equals("")) {
+			return "";
+		}
+		return str.substring(1);
 	}
 
 	public static int levenshtein(String word1, String word2) {
 		// Your code goes here
+
+		if (word2.length() == 0) {
+			return word1.length();
+		}
+		if (word1.length() == 0) {
+			return word2.length();
+		}
+
+		
+
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -24,12 +43,16 @@ public class SpellChecker {
 		In in = new In(fileName);
 
 		// Your code here
+		for (int i=0; i<3000; i++) {
+			dictionary[i] = in.readString();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
 		// Your code goes here
+		return "";
 	}
 
 }
